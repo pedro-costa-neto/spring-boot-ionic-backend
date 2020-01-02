f7d6239663ff16aa965f7dab6ba9ad436a56e376
Pedro Costa <pcon1996@gmail.com>
2020-01-02 19:37:17 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/pedro/cursomc/services/CategoriaService.java
+++ src/main/java/com/pedro/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.pedro.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.pedro.cursomc.domain.Categoria;
 import com.pedro.cursomc.repositories.CategoriaRepository;
+import com.pedro.cursomc.services.exception.DataIntegrityException;
 import com.pedro.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
@@ -31,5 +33,16 @@ public class CategoriaService {
 	public Categoria update( Categoria obj ) {
 		find( obj.getId() );
 		return repo.save( obj );
+	}
+	
+	public void delete( Integer id ) {
+		find( id );
+		
+		try {
+			repo.deleteById( id );
+		}catch ( DataIntegrityViolationException e ) {
+			throw new DataIntegrityException( "Não é possivel excluir uma categoria que possui produtos!" );
+		}
+		
 	}
 }
---
+++ src/main/java/com/pedro/cursomc/services/exception/DataIntegrityException.java
@@ -0,0 +1,14 @@
+package com.pedro.cursomc.services.exception;
+
+public class DataIntegrityException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public DataIntegrityException( String msg ) {
+		super( msg );
+	}
+	
+	public DataIntegrityException( String msg, Throwable ceuse ) {
+		super( msg, ceuse );
+	}
+}
