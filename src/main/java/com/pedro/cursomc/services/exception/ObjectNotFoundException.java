d1d57cea24845d061b8b4534b66d3766748f9a68
Pedro Costa <pcon1996@gmail.com>
2020-01-02 13:28:49 -0300

    Ajustando endpoint /categorias/{id}

---
+++ src/main/java/com/pedro/cursomc/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,14 @@
+package com.pedro.cursomc.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException( String msg ) {
+		super( msg );
+	}
+	
+	public ObjectNotFoundException( String msg, Throwable ceuse ) {
+		super( msg, ceuse );
+	}
+}
