3c2f77688cc8c48281fd4c77e7763423f248d8cf
Pedro Costa <pcon1996@gmail.com>
2020-01-03 11:39:27 -0300

    PUT, DELETE e GET para cliente

---
+++ src/main/java/com/pedro/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,64 @@
+package com.pedro.cursomc.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.pedro.cursomc.domain.Cliente;
+
+public class ClienteDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	
+	@NotEmpty ( message = "Preenchimento obrigatorio" )
+	@Length (min = 5, max = 120, message = "O tamanho deve ser entre 5 a 120 caracteres")
+	private String nome;
+	
+	@NotEmpty ( message = "Preenchimento obrigatorio" )
+	@Email ( message = "E-mail inválido" )
+	private String email;
+	
+	public ClienteDTO() {
+		
+	}
+
+	public ClienteDTO( Cliente obj ) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.email = obj.getEmail();
+	}
+
+
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
--- src/main/java/com/pedro/cursomc/services/ClienteService.java
+++ src/main/java/com/pedro/cursomc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.pedro.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.pedro.cursomc.domain.Cliente;
+import com.pedro.cursomc.dto.ClienteDTO;
 import com.pedro.cursomc.repositories.ClienteRepository;
+import com.pedro.cursomc.services.exception.DataIntegrityException;
 import com.pedro.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
@@ -21,5 +28,47 @@ public class ClienteService {
 		Optional<Cliente> obj = repo.findById( id );
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	
+	public Cliente insert( Cliente obj ) {
+		obj.setId( null );
+		return repo.save( obj );
+	}
+	
+	public Cliente update( Cliente obj ) {
+		Cliente newObj = find( obj.getId() );
+		updateData( newObj, obj );
+		return repo.save( newObj );
+	}
+	
+	public void delete( Integer id ) {
+		find( id );
+		
+		try {
+			repo.deleteById( id );
+		}catch ( DataIntegrityViolationException e ) {
+			throw new DataIntegrityException( "Não é possivel excluir porque há entidades relacionadas!" );
+		}
+		
+	}
+	
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+	
+	@SuppressWarnings("deprecation")
+	public Page<Cliente> findPage( Integer page, Integer linesPage, String orderBy, String direction ){
+		PageRequest pageRequest = new PageRequest( page, linesPage, Direction.valueOf( direction ), orderBy );
+		return repo.findAll( pageRequest );
+	}
+	
+	public Cliente fromDTO( ClienteDTO objDto) {
+		return new Cliente( objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	private void updateData( Cliente newObj, Cliente obj ) {
+		newObj.setNome( obj.getNome() );
+		newObj.setEmail( obj.getEmail() );
 	}
 }
