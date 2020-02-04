1cd1c319ee12e277ae9d54811e5494435d2008f9
Pedro Costa <pcon1996@gmail.com>
2020-02-03 22:27:28 -0300

    Implementando autorização

--- src/main/java/com/pedro/cursomc/config/SecurityConfig.java
+++ src/main/java/com/pedro/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.pedro.cursomc.security.JWTAuthenticationFilter;
+import com.pedro.cursomc.security.JWTAuthorizationFilter;
 import com.pedro.cursomc.security.JWTUtil;
 
 @Configuration
@@ -56,6 +57,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(HttpMethod.GET, PUBLIC_MAICHERS_GET).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
