5bf369b0326eaf9e118423358309be7712285599
Pedro Costa <pcon1996@gmail.com>
2020-02-04 07:59:20 -0300

    Implementando: Esqueci a senha

---
+++ src/main/java/com/pedro/cursomc/dto/EmailDTO.java
@@ -0,0 +1,26 @@
+package com.pedro.cursomc.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+public class EmailDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@NotEmpty ( message = "Preenchimento obrigatorio" )
+	@Email ( message = "E-mail inválido" )
+	private String email;
+	
+	public EmailDTO() {
+		
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
--- src/main/java/com/pedro/cursomc/resources/AuthResources.java
+++ src/main/java/com/pedro/cursomc/resources/AuthResources.java
@@ -1,15 +1,19 @@
 package com.pedro.cursomc.resources;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pedro.cursomc.dto.EmailDTO;
 import com.pedro.cursomc.security.JWTUtil;
 import com.pedro.cursomc.security.UserSS;
+import com.pedro.cursomc.services.AuthService;
 import com.pedro.cursomc.services.UserServices;
 
 @RestController
@@ -18,6 +22,9 @@ public class AuthResources {
 	
 	@Autowired
 	private JWTUtil jwtUtil;
+	
+	@Autowired
+	private AuthService service;
 
 	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
@@ -25,6 +32,12 @@ public class AuthResources {
 		String token = jwtUtil.generateToken(user.getUsername());
 		response.addHeader("Authorization", "Bearer " + token);
 		response.addHeader("access-control-expose-headers", "Authorization");
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDTO) {
+		service.sendNewPassword(objDTO.getEmail());
 		return ResponseEntity.noContent().build();
 	}
 	
