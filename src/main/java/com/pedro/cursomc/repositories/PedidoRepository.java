bff7bc80a11ed00e5dad344d7f256617c71187bf
Pedro Costa <pcon1996@gmail.com>
2020-02-04 07:33:22 -0300

    Restrição de conteudo: cliente recupera os seus pedidos

--- src/main/java/com/pedro/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/pedro/cursomc/repositories/PedidoRepository.java
@@ -1,11 +1,17 @@
 package com.pedro.cursomc.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.pedro.cursomc.domain.Cliente;
 import com.pedro.cursomc.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
-
+	
+	@Transactional(readOnly=true)
+	Page<Pedido> findByCliente(Cliente cliente, PageRequest pageRequest);
 }
--- src/main/java/com/pedro/cursomc/services/PedidoService.java
+++ src/main/java/com/pedro/cursomc/services/PedidoService.java
@@ -4,8 +4,12 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.pedro.cursomc.domain.Cliente;
 import com.pedro.cursomc.domain.ItemPedido;
 import com.pedro.cursomc.domain.PagamentoComBoleto;
 import com.pedro.cursomc.domain.Pedido;
@@ -13,6 +17,8 @@ import com.pedro.cursomc.domain.enums.EstadoPagamento;
 import com.pedro.cursomc.repositories.ItemPedidoRepository;
 import com.pedro.cursomc.repositories.PagamentoRepository;
 import com.pedro.cursomc.repositories.PedidoRepository;
+import com.pedro.cursomc.security.UserSS;
+import com.pedro.cursomc.services.exception.AuthorizationException;
 import com.pedro.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
@@ -74,5 +80,15 @@ public class PedidoService {
 		
 		return obj;
 		
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserServices.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
